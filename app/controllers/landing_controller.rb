3568befa9bee9bc0162201f78875ffced4b5f096
Vlasov Viktor <vvvlasov@activebc.ru>
2019-04-23 00:47:54 +0300

    config

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,8 +1,10 @@
 class LandingController < ApplicationController
   def index
     if params[:translit].present?
-      @mode = params.dig(:translit, :mode)
-      @translitted_phrase = Kirillica.translit(params.dig(:translit, :phrase))
+      @mode = params.dig(:translit, :mode) || :iso_9
+      @phrase = params.dig(:translit, :phrase)
+      puts @mode.to_sym
+      @translitted_phrase = Kirillica.translit(@phrase, @mode.to_sym)
     end
   end
 end
